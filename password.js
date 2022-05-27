b2c9a3d35196b6d6f0734021ff44e69a6223038a
Grayson Allen <grayallen1991@gmail.com>
2022-05-27 18:53:22 -0400

    added password.js notes/code

--- password.js
+++ password.js
@@ -29,3 +29,51 @@ if (passwordLength < 10){
 	reader.close()
 
 });
+
+
+
+
+// console.log("Welcome to the password validator tool")
+
+// const readline = require('readline');
+
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+
+
+// reader.question("Type the password you want validated: \n", function(input){
+
+//     console.log('the input was ' + input)
+
+//     // if(input.length >= 10){
+//     //     console.log('Good Password')
+//     // } else {
+//     //     console.log('Bad password: your password needs to be at least 10 characters long')
+//     // }
+
+//     const errorArr = [];
+
+//     if (input.length < 10){
+//         errorArr.push('your password needs to be at least 10 characters long')
+//     }
+
+//     if (!/\d/.test(input)) {
+//         errorArr.push('your password needs a number')
+//     }
+
+//     if (errorArr.length === 0) {
+//         console.log('Good password!')
+//     } else {
+//         console.log('Bad password because:')
+//         for (let i =0; i < errorArr.length; i++) {
+//             console.log(errorArr[i])
+//         }
+//     }
+    
+    
+//     reader.close()
+	
+// });
